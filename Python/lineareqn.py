f498ce166d6c5e500658963de6e29f450ad52a67
Chi George Mofor <chigeorgemofor@gmail.com>
2024-10-22 00:30:12 +0100

    Solving a linear equation

---
+++ Python/lineareqn.py
@@ -0,0 +1,10 @@
+a = float(input('a = '))
+b = float(input('b = '))
+if a == 0:
+    if b == 0:
+        print('Identity Equation')
+    else:
+        print('Contrary Equation')
+else:
+    x = -b/a
+    print('x =', x)
