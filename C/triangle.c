ba1aadfec7b40ce5e296c638928f570918869d13
ChiGeorgeMofor <105949693+ChiGeorgeMofor@users.noreply.github.com>
2024-11-26 00:02:56 +0100

    Update triangle.c

--- C/triangle.c
+++ C/triangle.c
@@ -11,7 +11,7 @@ int main() {
 
         if (base > 0 && height > 0) {
             for (int i = 1; i <= height; i++) {
-                for (int j = 1; j <= base; j++) {
+                for (int j = 1; j <= base; j++) { 
                     if (j <= i) {
                         printf("* ");
                     }
